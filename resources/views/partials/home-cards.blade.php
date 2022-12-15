bd0d7f65292c57000a5a4678bf7a74f41b4931b4
BetterCallAle <dap22091998@gmail.com>
2022-12-15 18:52:07 +0100

    added alt

--- resources/views/partials/home-cards.blade.php
+++ resources/views/partials/home-cards.blade.php
@@ -1,8 +1,8 @@
 <div class="col">
-    <div class="ms_card">
+    <div class="ms_card py-4">
         <a href="">
             <div class="ms_card-img">
-                <img src="{{ $comic["thumb"] }}" alt="">
+                <img src="{{ $comic["thumb"] }}" alt="Copertina di {{$comic["title"]}}">
             </div>
 
             <div class="ms_card-txt">
